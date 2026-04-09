39d7c97330cbcbf50fd45d73cf8e67a7b025082d
Alisson Carenhatto <alissoncarenhatto@gmail.com>
2026-04-09 19:09:20 -0300

    endpoint teste

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { Public } from './public-decorator';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Public } from "./public-decorator";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
-  @Post('login')
+  @Post("login")
   login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
+  }
+
+  @Get("test")
+  test() {
+    return { ok: true };
   }
 }
